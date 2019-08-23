86b837e1fe0fd25a7f97c7a1653bdf057daa092d
KwonJiYong <48173172+KwonJiYongYG@users.noreply.github.com>
2019-08-23 15:17:01 +0800

    优化代码

--- spring-cloud-netflix-consumer/src/main/java/com/framework/consumer/service/TestService.java
+++ spring-cloud-netflix-consumer/src/main/java/com/framework/consumer/service/TestService.java
@@ -1,6 +1,6 @@
 package com.framework.consumer.service;
 
-import com.framework.consumer.service.hystrix.TestServiceHystrix;
+import com.framework.consumer.service.hystrix.TestServiceHystrixImpl;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,12 +13,22 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @createTime: 2019-08-17 01:25
  **/
 @RequestMapping("/provider")
-@FeignClient(value = "spring-cloud-netflix-provider", fallback = TestServiceHystrix.class)
+@FeignClient(value = "spring-cloud-netflix-provider", fallback = TestServiceHystrixImpl.class)
 public interface TestService {
 
+    /**
+     * 测试
+     * @param message message
+     * @return String
+     */
     @GetMapping("/hi")
     String sayHello(@RequestParam("message") String message);
 
+    /**
+     * 测试
+     * @param name name
+     * @return String
+     */
     @GetMapping("/hi/{name}")
     String sayHi(@PathVariable("name") String name);
 
